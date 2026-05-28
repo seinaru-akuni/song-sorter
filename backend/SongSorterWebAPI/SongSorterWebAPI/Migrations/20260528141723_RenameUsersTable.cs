ebe0c1c04d78a08a883c8ccf55faf2513a29606c
akuni <seinaruakuni@gmail.com>
2026-05-28 18:25:09 +0200

    feat(database): add GoogleUser table

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Migrations/20260528141723_RenameUsersTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SongSorterWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "GoogleUsers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GoogleUsers",
+                table: "GoogleUsers",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GoogleUsers",
+                table: "GoogleUsers");
+
+            migrationBuilder.RenameTable(
+                name: "GoogleUsers",
+                newName: "Users");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
