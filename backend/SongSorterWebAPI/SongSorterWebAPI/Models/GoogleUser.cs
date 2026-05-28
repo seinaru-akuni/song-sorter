25bae8dcdd5d01711a9f300db3d721972b79545d
akuni <seinaruakuni@gmail.com>
2026-05-28 18:05:28 +0200

    add GoogleUSer model

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Models/GoogleUser.cs
@@ -0,0 +1,18 @@
+﻿namespace SongSorterWebAPI.Models
+{
+    public class GoogleUser
+    {
+        public int Id { get; set; }
+
+        // Унікальний ID, який прийде від Google (щоб ми завжди впізнавали користувача)
+        public required string GoogleId { get; set; }
+
+        public required string Email { get; set; }
+
+        // Ось куди ми сховаємо наш довгостроковий ключ!
+        public string? RefreshToken { get; set; }
+
+        // Дата, коли токен перестане діяти (щоб знати, коли просити логін заново)
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+    }
+}
