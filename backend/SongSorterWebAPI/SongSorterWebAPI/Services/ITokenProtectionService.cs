773f2e4457b2bab46f258b300b1d6ef0372caf73
akuni <seinaruakuni@gmail.com>
2026-05-28 18:29:00 +0200

    feat(user): add TokenProtection Service

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/ITokenProtectionService.cs
@@ -0,0 +1,8 @@
+﻿namespace SongSorterWebAPI.Services
+{
+    public interface ITokenProtectionService
+    {
+        string EncryptToken(string token);
+        string DecryptToken(string encryptedToken);
+    }
+}
---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/TokenProtectionService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace SongSorterWebAPI.Services
+{
+    public class TokenProtectionService : ITokenProtectionService
+    {
+        private readonly IDataProtector _protector;
+
+        public TokenProtectionService(IDataProtectionProvider provider)
+        {
+            _protector = provider.CreateProtector("SongSorter.GoogleTokens");
+        }
+
+        public string EncryptToken(string token) => _protector.Protect(token);
+
+        public string DecryptToken(string encryptedToken) => _protector.Unprotect(encryptedToken);
+    }
+}
+
