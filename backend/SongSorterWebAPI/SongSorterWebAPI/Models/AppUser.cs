d468d51c3091bb6185ad90b0057a6717a7e4515e
akuni <seinaruakuni@gmail.com>
2026-05-29 14:27:31 +0200

    fetch(database): redesign authentication schema

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿namespace SongSorterWebAPI.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        // Головна електронна пошта для входу на твій сайт
+        public required string Email { get; set; }
+
+        // Пароль для майбутньої локальної реєстрації (поки що буде NULL)
+        public string? PasswordHash { get; set; }
+
+        // Навігаційна властивість для EF Core: 
+        // Вказує, що у цього користувача може бути багато підключених інтеграцій
+        public List<LinkedAccount> LinkedAccounts { get; set; } = new();
+    }
+}
