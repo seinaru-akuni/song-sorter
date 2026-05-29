b2eb5acbb81212827d1222f1db16b779adbd2643
akuni <seinaruakuni@gmail.com>
2026-05-29 16:19:37 +0200

    fetch(dto): add logind and register DTOs

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SongSorterWebAPI.DTOs
+{
+    public class RegisterDto
+    {
+        public required string Email { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmPassword { get; set; }
+    }
+}
