d5da37489f5030c32867f01482b1f695a104e5bf
akuni <seinaruakuni@gmail.com>
2026-05-31 15:07:55 +0200

    feat(services): add User Service

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using SongSorterWebAPI.Data;
+using SongSorterWebAPI.Models;
+
+namespace SongSorterWebAPI.Services
+{
+    public interface IUserService
+    {
+        public Task<bool> IsEmailTakenAsync(string email);
+
+        public Task<bool> IsUsernameTakenAsync(string username);
+        public Task<int> ContextSaveChangesAsync();
+        public void AddNewAppUser(AppUser newUser);
+    }
+}
---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/UserService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SongSorterWebAPI.Data;
+using SongSorterWebAPI.Models;
+using System.Security.Cryptography;
+
+namespace SongSorterWebAPI.Services
+{
+    public class UserService : IUserService
+    {
+        readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }        
+
+        public async Task<bool> IsEmailTakenAsync(string email)
+        {
+            if(await _context.AppUsers.AnyAsync(u => u.Email == email))
+                return true;
+            else return false;
+        }
+
+        public async Task<bool> IsUsernameTakenAsync(string username)
+        {
+            if (await _context.AppUsers.AnyAsync(u => u.Username == username))
+                return true;
+            else return false;
+        }
+
+        public void AddNewAppUser(AppUser newUser)
+        {
+            _context.AppUsers.Add(newUser);
+        }
+
+        public async Task<int> ContextSaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
