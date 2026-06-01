afe3e081097997a4085f181a51bd2aa37df986e7
akuni <seinaruakuni@gmail.com>
2026-06-01 15:40:32 +0200

    feat(pages): add Home, Registeer, Login pages

---
+++ frontend/song-sorter-frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom";
+
+function LoginPage() {
+    return (
+        <div>
+            <h1>Login to the Music App</h1>
+            <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link> to continue.</p>
+        </div>
+    )
+}
+
+export default LoginPage;
