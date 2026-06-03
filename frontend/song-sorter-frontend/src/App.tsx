20e832a6416ed86a727dc6bef03fb9ee30b6ef27
akuni <seinaruakuni@gmail.com>
2026-06-03 18:03:14 +0200

    feat(app): remove navbar and theme toggle

--- frontend/song-sorter-frontend/src/App.tsx
+++ frontend/song-sorter-frontend/src/App.tsx
@@ -5,36 +5,17 @@ import {useState, useEffect} from "react";
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import Navbar from './components/NavBar';
+
 
 
 function App() {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light');
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null;
-    if (savedTheme) {
-      setTheme(savedTheme);
-      document.documentElement.classList.toggle('dark', savedTheme === 'dark');
-    }
-  }, []);
-
-  function toggleTheme() {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-    document.documentElement.classList.toggle('dark', newTheme === 'dark');
-  }
 
   return (
     <div>
       <BrowserRouter>
         
-        <nav className="dark:bg-gray-800 dark:text-white" style={{ padding: '10px', borderBottom: '1px solid #ccc', marginBottom: '20px' }}>
-          <Link to="/" style={{ marginRight: '15px' }}>Головна</Link>
-          <Link to="/login" style={{ marginRight: '15px' }}>Вхід</Link>
-          <Link to="/register">Реєстрація</Link>
-          <button onClick={toggleTheme}>{theme === 'light' ? <Moon /> : <Sun />}</button>
-        </nav>
+        <Navbar />
 
         {/* Routes - це місце, де будуть підмінятися сторінки */}
         <Routes>
