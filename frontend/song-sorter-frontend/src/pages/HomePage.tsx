a5663a601ee5bdb2567349f9e2408560d6eabed0
akuni <seinaruakuni@gmail.com>
2026-06-03 18:07:23 +0200

    fix: remove everything and leave page blank

--- frontend/song-sorter-frontend/src/pages/HomePage.tsx
+++ frontend/song-sorter-frontend/src/pages/HomePage.tsx
@@ -4,11 +4,7 @@ import {Link} from "react-router-dom";
 function HomePage() {
     return (
         <div>
-            <h1>Welcome to the Music App</h1>
-            <h1 className="text-4xl font-bold text-blue-500">
-                Tailwind працює!
-            </h1>
-            <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link> to continue.</p>
+            
         </div>
     )
 }
