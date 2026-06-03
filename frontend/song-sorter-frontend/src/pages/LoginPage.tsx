43a82495690689106a883248376024575e25331b
akuni <seinaruakuni@gmail.com>
2026-06-03 18:07:59 +0200

    fix: add login form

--- frontend/song-sorter-frontend/src/pages/LoginPage.tsx
+++ frontend/song-sorter-frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom";
+import LoginForm from "../components/LoginForm";
 
 function LoginPage() {
     return (
         <div>
             <h1>Login to the Music App</h1>
+            <LoginForm />
             <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link> to continue.</p>
         </div>
     )
