116d5b607dc994a7182fd60b9139f511584df52c
akuni <seinaruakuni@gmail.com>
2026-06-04 14:03:06 +0200

    fix: remove trash

--- frontend/song-sorter-frontend/src/pages/LoginPage.tsx
+++ frontend/song-sorter-frontend/src/pages/LoginPage.tsx
@@ -4,9 +4,9 @@ import LoginForm from "../components/LoginForm";
 function LoginPage() {
     return (
         <div>
-            <h1>Login to the Music App</h1>
-            <LoginForm />
-            <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link> to continue.</p>
+            <div className="flex justify-center">
+                <LoginForm />
+            </div>
         </div>
     )
 }
