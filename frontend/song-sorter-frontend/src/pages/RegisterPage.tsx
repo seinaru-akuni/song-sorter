01e0381fa370b5bde48f3e906ff1b94f44bdbeab
akuni <seinaruakuni@gmail.com>
2026-06-04 14:03:21 +0200

    fix: remove trash

--- frontend/song-sorter-frontend/src/pages/RegisterPage.tsx
+++ frontend/song-sorter-frontend/src/pages/RegisterPage.tsx
@@ -4,10 +4,9 @@ import RegisterForm from "../components/RegisterForm";
 function RegisterPage() {
     return (
         <div>
-            <h1>Register for the Music App</h1>
-            <RegisterForm />
-
-            <p>Please <Link to="/register">register</Link> or <Link to="/login">login</Link> to continue.</p>
+            <div className="flex justify-center">
+                <RegisterForm />
+            </div>
         </div>
     )
 }
