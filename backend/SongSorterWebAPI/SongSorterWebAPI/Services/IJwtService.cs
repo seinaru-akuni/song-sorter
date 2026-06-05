e86154283be3ea470f83b6328c29a1279026c5e1
akuni <seinaruakuni@gmail.com>
2026-06-05 19:05:24 +0200

    Reapply "feat: add remember me logic"
    This reverts commit 21af2ab6bb2fa01455c66383d1c85d6e6514786a.

--- backend/SongSorterWebAPI/SongSorterWebAPI/Services/IJwtService.cs
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/IJwtService.cs
@@ -3,6 +3,6 @@
     public interface IJwtService
     {
         // Метод приймає ID користувача та контекст поточного HTTP-запиту (щоб мати доступ до кукі)
-        void GenerateAndSetTokenCookie(int userId, HttpContext httpContext);
+        void GenerateAndSetTokenCookie(int userId, HttpContext httpContext, bool rememberMe);
     }
 }
--- backend/SongSorterWebAPI/SongSorterWebAPI/Services/JwtService.cs
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Services/JwtService.cs
@@ -15,10 +15,12 @@ namespace SongSorterWebAPI.Services
             _config = config;
         }
 
-        public void GenerateAndSetTokenCookie(int userId, HttpContext httpContext)
+        public void GenerateAndSetTokenCookie(int userId, HttpContext httpContext, bool rememberMe)
         {
             // 1. Створюємо набір даних (Claims), які будуть зашифровані в токені
             // Ми ховаємо туди лише ID користувача (Sub)
+            var tokenExpiry = rememberMe ? DateTime.UtcNow.AddDays(30) : DateTime.UtcNow.AddDays(1);
+
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
@@ -32,7 +34,7 @@ namespace SongSorterWebAPI.Services
             // 3. Збираємо сам токен (термін дії, наприклад, 7 днів)
             var token = new JwtSecurityToken(
                 claims: claims,
-                expires: DateTime.UtcNow.AddDays(7),
+                expires: tokenExpiry,
                 signingCredentials: creds
             );
 
@@ -45,7 +47,6 @@ namespace SongSorterWebAPI.Services
                 HttpOnly = true, // Забороняє доступ до куки з JavaScript (захист від XSS-атак)
                 Secure = true,   // Вимагає HTTPS (на localhost працює нормально)
                 SameSite = SameSiteMode.Strict, // Захист від CSRF-атак (кука не відправляється з інших сайтів)
-                Expires = DateTime.UtcNow.AddDays(7)
             };
 
             // Додаємо куку до відповіді сервера. 
