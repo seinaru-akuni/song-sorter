004ce9996d47be8e625b2637de78e00444cf8b59
akuni <seinaruakuni@gmail.com>
2026-06-10 21:51:16 +0300

    feat(database): add isEmailVerifyed, VerificationCode, VerificationCodeExpiry fields to AppUsers table

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/Migrations/20260610181030_AddEmailVerificationFields.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SongSorterWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEmailVerified",
+                table: "AppUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VerificationCode",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "VerificationCodeExpiry",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEmailVerified",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCode",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "VerificationCodeExpiry",
+                table: "AppUsers");
+        }
+    }
+}
