9c5b012258d251396e7956cb49a858fa4abaad46
akuni <seinaruakuni@gmail.com>
2026-06-10 21:52:16 +0300

    feat(DTO): add cerify email dto

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/DTOs/VerifyEmailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SongSorterWebAPI.DTOs
+{
+    public class VerifyEmailDto
+    {
+        public required string Email { get; set; }
+        public required string Code { get; set; }
+    }
+}
