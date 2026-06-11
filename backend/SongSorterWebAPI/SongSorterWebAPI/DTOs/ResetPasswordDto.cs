12beb0a5d31197dcf5e6c73529a73fdfbe5abd7b
akuni <seinaruakuni@gmail.com>
2026-06-11 19:40:23 +0300

    feat(DTO): create ResetPasswordDto

---
+++ backend/SongSorterWebAPI/SongSorterWebAPI/DTOs/ResetPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SongSorterWebAPI.DTOs
+{
+    public class ResetPasswordDto
+    {
+        public string Email {  get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
+    }
+}
